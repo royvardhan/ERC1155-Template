befad4029f7bca27ab233c5e06843ba1453e59e9
royvardhan <royvardhan.official@gmail.com>
2022-10-15 18:45:13 +0530

    .

--- erc1155.sol
+++ erc1155.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract FNFT is ERC1155, Ownable {
+contract ERC1155Example is ERC1155, Ownable {
     
   string public name;
   string public symbol;
